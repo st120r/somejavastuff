319d0affe502308f9b253cd8c3950aa5f5a64827
anton.kulikov <anton.kulikov@x5.ru>
2023-07-11 20:39:27 +0300

    .

--- demo/twoSum.java
+++ demo/twoSum.java
@@ -5,11 +5,11 @@ public class twoSum {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
+                    return new int[]{i, j};
                 }
             }
         }
-        return new int[] {};
+        return new int[]{};
     }
 
     /**
