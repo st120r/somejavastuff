ee555f813cfc39025a840b2fb494bbfac1dfeaf4
anton.kulikov <anton.kulikov@x5.ru>
2023-07-11 20:32:01 +0300

    .

---
+++ demo/validparentheses.java
@@ -0,0 +1,60 @@
+package demo;
+
+import java.util.Stack;
+
+public class validparentheses {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+
+
+    /**
+     * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+     *
+     * An input string is valid if:
+     *
+     * Open brackets must be closed by the same type of brackets.
+     * Open brackets must be closed in the correct order.
+     * Every close bracket has a corresponding open bracket of the same type.
+     *
+     *
+     * Example 1:
+     *
+     * Input: s = "()"
+     * Output: true
+     * Example 2:
+     *
+     * Input: s = "()[]{}"
+     * Output: true
+     * Example 3:
+     *
+     * Input: s = "(]"
+     * Output: false
+     *
+     *
+     * Constraints:
+     *
+     * 1 <= s.length <= 104
+     * s consists of parentheses only '()[]{}'.
+     */
+}
